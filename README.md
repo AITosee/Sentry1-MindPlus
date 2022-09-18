d70c50fd0dcb848913d8de8d0145c481ff77409f
Uniquemf <weiyanfengv@gmail.com>
2022-09-18 15:37:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ext-sentry
+
+欢迎使用 [ext-sentry](https://github.com/AITosee/ext-sentry) for Mind+ 库，可点击[查看文档](https://tosee.readthedocs.io/zh/latest/Sentry/MindPlus/index.html)。
+
+## 用作扩展
+
+此仓库可以作为 **插件** 添加到 Mind+ 中。
+
+1. 打开 Mind+
+1. 点击 **新项目**  **上传模式**
+1. 点击齿轮图标菜单下的 **扩展** **用户库**
+1. 搜索 **https://github.com/AITosee/ext-sentry** 并导入
---
+++ arduinoC/main.ts
@@ -0,0 +1,179 @@
+
+
+//% color="#ff9f06" iconWidth=50 iconHeight=40
+namespace Sentry1 {
+    //% block="Sentry1 init port [MODE] addr [ADDR]" blockType="command"
+    //% MODE.shadow="dropdown" MODE.options="MODE"
+    //% ADDR.shadow="dropdown" ADDR.options="SENTRY"
+    export function Begin(parameter: any) {
+        let mode = parameter.MODE.code;
+        let addr = parameter.ADDR.code;
+
+        Generator.addInclude("ArduinoInclude", "#include <Arduino.h>");
+        Generator.addInclude("SentryInclude", "#include <Sentry.h>");
+
+        Generator.addObject(`sentry1.Object`, "Sentry1", `sentry1(${addr});`);
+
+        if (mode == 'Wire') {
+            Generator.addInclude("WireInclude", "#include <Wire.h>");
+            Generator.addSetupMainTop("Wire.begin", `Wire.begin();`);
+        }
+
+        Generator.addCode(`while (SENTRY_OK != sentry1.begin(&${mode})) {yield();}`);
+    }
+
+    //% block="Sentry1 [VISION_STA] vision [VISION_TYPE]" blockType="command"
+    //% VISION_TYPE.shadow="dropdown" VISION_TYPE.options="VISION"
+    //% VISION_STA.shadow="dropdown" VISION_STA.options="VISION_STA"    
+    export function VisionSet(parameter: any) {
+
+        let vision_type = parameter.VISION_TYPE.code;
+        let vision_sta = parameter.VISION_STA.code;
+        if (vision_sta == "ON") {
+            Generator.addCode(`sentry1.VisionBegin(${vision_type});`);
+        } else {
+            Generator.addCode(`sentry1.VisionEnd(${vision_type});`);
+        }
+
+    }
+
+    //% block="Sentry1 set [VISION_TYPE] Param [NUM]" blockType="command"
+    //% VISION_TYPE.shadow="dropdown" VISION_TYPE.options="VISION"
+    //% NUM.shadow="range"   NUM.params.min=1    NUM.params.max=25    NUM.defl=1
+    export function SetParamNum(parameter: any) {
+
+        let vision_type = parameter.VISION_TYPE.code;
+        let num = parameter.NUM.code;
+        Generator.addCode(`sentry1.SetParamNum(${vision_type},(int)${num});`);
+    }
+
+    //% block="Sentry1 set color parameter [NUM] ROI area center point abscissa [XVALUE] ordinate [YVALUE] width [WIDTH] height [HIGHT]"
+    //% NUM.shadow="range"   NUM.params.min=1    NUM.params.max=25    NUM.defl=1
+    //% XVALUE.shadow="number"   XVALUE.defl=160
+    //% YVALUE.shadow="number"   YVALUE.defl=120
+    //% WIDTH.shadow="number"   WIDTH.defl=8
+    //% HIGHT.shadow="number"   HIGHT.defl=8
+    export function SetColorParam(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let x = parameter.XVALUE.code;
+        let y = parameter.YVALUE.code;
+        let w = parameter.WIDTH.code;
+        let h = parameter.HIGHT.code;
+
+        Generator.addObject("param_obj", "sentry_object_t", `param;`);
+        Generator.addCode(`param.x_value = ${x};`);
+        Generator.addCode(`param.y_value = ${y};`);
+        Generator.addCode(`param.width = ${w};`);
+        Generator.addCode(`param.height = ${h};`);
+        Generator.addCode(`sentry1.SetParam(Sentry1::kVisionColor,&param,(int)${num});`);
+    }
+
+    //% block="Sentry1 set color block detection parameter [NUM] minimum width [WIDTH] minimum height [HIGHT] to detect color [COLOR_LABLE]" blockType="command"
+    //% NUM.shadow="range"   NUM.params.min=1    NUM.params.max=25    NUM.defl=1
+    //% WIDTH.shadow="number"   WIDTH.defl=8
+    //% HIGHT.shadow="number"   HIGHT.defl=8
+    //% COLOR_LABLE.shadow="dropdown" COLOR_LABLE.options="COLOR_LABLE"
+    export function SetBlobParam(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let l = parameter.COLOR_LABLE.code;
+        let w = parameter.WIDTH.code;
+        let h = parameter.HIGHT.code;
+
+        Generator.addObject("param_obj", "sentry_object_t", `param;`);
+        Generator.addCode(`param.width = ${w};`);
+        Generator.addCode(`param.height = ${h};`);
+        Generator.addCode(`param.label = ${l};`);
+        Generator.addCode(`sentry1.SetParam(Sentry1::kVisionBlob,&param,(int)${num});`);
+    }
+
+    //% block="Sentry1 Set the LED algorithm to detect a color of [LED_COLOR1] and not to detect a color of [LED_COLOR2]" blockType="command"
+    //% SENTRY.shadow="dropdown" SENTRY.options="SENTRY"
+    //% LED_COLOR1.shadow="dropdown" LED_COLOR1.options="LED_COLOR"
+    //% LED_COLOR2.shadow="dropdown" LED_COLOR2.options="LED_COLOR"     
+    export function LedSetColor(parameter: any) {
+        let color1 = parameter.LED_COLOR1.code;
+        let color2 = parameter.LED_COLOR2.code;
+        Generator.addCode(`sentry1.LedSetColor(${color1},${color2});`);
+    }
+
+    //% block="Sentry1 Set camera [AWB]" blockType="command"
+    //% SENTRY.shadow="dropdown" SENTRY.options="SENTRY"
+    //% AWB.shadow="dropdown" AWB.options="AWB" 
+    export function CameraSetAwb(parameter: any) {
+        let awb = parameter.AWB.code;
+        Generator.addCode(`sentry1.CameraSetAwb(${awb});`);
+    }
+
+    //% block="Sentry1 get vision [VISION_TYPE] status" blockType="reporter"
+    //% VISION_TYPE.shadow="dropdown" VISION_TYPE.options="VISION"    
+    export function GetVisionResult(parameter: any) {
+
+        let vision_type = parameter.VISION_TYPE.code;
+        Generator.addCode([`sentry1.GetValue(${vision_type}, kStatus)`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+
+    //% block="Sentry1 get [VISION_TYPE] [VISION_ID] [OBJ_INFO]" blockType="reporter"
+    //% VISION_TYPE.shadow="dropdown" VISION_TYPE.options="VISION"
+    //% VISION_ID.shadow="number"  VISION_ID.defl=1
+    //% OBJ_INFO.shadow="dropdown" OBJ_INFO.options="OBJ_INFO"    
+    export function GetValue(parameter: any) {
+
+        let vision_type = parameter.VISION_TYPE.code;
+        let num = parameter.VISION_ID.code;
+        let obj = parameter.OBJ_INFO.code;
+        Generator.addCode([`sentry1.GetValue(${vision_type},${obj},(int)${num})`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+
+
+    //% block="Sentry1 get Color [NUM] [OBJ_RGB_INFO]" blockType="reporter"
+    //% NUM.shadow="number" NUM.defl=1
+    //% OBJ_RGB_INFO.shadow="dropdown" OBJ_RGB_INFO.options="OBJ_RGB_INFO"    
+    export function GetColorValue(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let obj = parameter.OBJ_RGB_INFO.code;
+        Generator.addCode([`sentry1.GetValue(Sentry1::kVisionColor,${obj},(int)${num})`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+
+    //% block="Sentry1 get Line [NUM] [OBJ_LINE_INFO]" blockType="reporter"
+    //% NUM.shadow="number" NUM.defl=1
+    //% OBJ_LINE_INFO.shadow="dropdown" OBJ_LINE_INFO.options="OBJ_LINE_INFO"    
+    export function GetLineValue(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let obj = parameter.OBJ_LINE_INFO.code;
+        Generator.addCode([`sentry1.GetValue(Sentry1::kVisionLine,${obj},(int)${num})`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+
+    //% block="Sentry1 Color detected [NUM] [COLOR_LABLE]" blockType="boolean"
+    //% NUM.shadow="number" NUM.defl=1
+    //% COLOR_LABLE.shadow="dropdown" COLOR_LABLE.options="COLOR_LABLE"    
+    export function GetColorLable(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let obj = parameter.COLOR_LABLE.code;
+        Generator.addCode([`sentry1.GetValue(Sentry1::kVisionColor,kLabel,(int)${num})==${obj}`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+
+    //% block="Sentry1 Blob detected [NUM] [COLOR_LABLE]" blockType="boolean"
+    //% NUM.shadow="number" NUM.defl=1
+    //% COLOR_LABLE.shadow="dropdown" COLOR_LABLE.options="COLOR_LABLE"    
+    export function GetColorBlob(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let obj = parameter.COLOR_LABLE.code;
+        Generator.addCode([`sentry1.GetValue(Sentry1::kVisionBlob,kLabel,(int)${num})==${obj}`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+
+    //% block="Sentry1 Card detected [NUM] [CARD_LABLE]" blockType="boolean"
+    //% NUM.shadow="number" NUM.defl=1
+    //% CARD_LABLE.shadow="dropdown" CARD_LABLE.options="CARD_LABLE"    
+    export function GetCardLable(parameter: any) {
+
+        let num = parameter.NUM.code;
+        let obj = parameter.CARD_LABLE.code;
+        Generator.addCode([`sentry1.GetValue(Sentry1::kVisionCard,kLabel,(int)${num})==${obj}`, Generator.ORDER_UNARY_POSTFIX]);
+    }
+}
